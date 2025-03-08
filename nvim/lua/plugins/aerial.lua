1dde46484199cc53e444911495d97216e46221a3
Tatsuya Otaki <tatsuya.otaki@outlook.com>
2025-03-08 11:03:39 +0000

    cleaning up aerial setup tiny bit

--- nvim/lua/plugins/aerial.lua
+++ nvim/lua/plugins/aerial.lua
@@ -2,9 +2,7 @@ return {
 	"stevearc/aerial.nvim",
 	config = function()
 		local aerial = require("aerial")
-		aerial.setup({
-			on_attach = function(bufnr) end,
-		})
+		aerial.setup({})
 	end,
 	vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>", { desc = "Toggle Aerial" }),
 }
