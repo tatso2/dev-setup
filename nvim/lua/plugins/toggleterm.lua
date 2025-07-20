75d8ac167d9e11c35d489d7b603e89ce718861e9
Tatsuya Otaki <tatsuya.otaki@outlook.com>
2025-07-20 13:53:29 +0100

    New terminal configuration

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -2,14 +2,24 @@ return {
 	"akinsho/toggleterm.nvim",
 	version = "*",
 	opts = {
-		direction = "float",
+		direction = "horizontal",
 		open_mapping = [[<c-`>]],
 		float_opts = {
 			border = "single",
 		},
 	},
 	config = function(_, opts)
+		local keymap = vim.keymap -- for conciseness
+		local trim_spaces = true
 		local toggleterm = require("toggleterm")
+
+		-- Keymaps
+		-- To allow referencing code space on terminal
+		keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit the insert mode in terminal" })
+		keymap.set("v", "<space>s", function()
+			toggleterm.send_lines_to_terminal("visual_selection", trim_spaces, { args = vim.v.count })
+		end, { desc = "Send line to terminal" })
+
 		toggleterm.setup(opts)
 	end,
 }
